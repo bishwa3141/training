48d7893ba6c055dd66117f678eb8a613f5128d3c
bishwa3141 <bishwa.silwal@bison.howard.edu>
2015-04-04 01:02:12 -0400

    Learnt to use multiple classes in java

---
+++ src/training/new_clas.java
@@ -0,0 +1,12 @@
+package training;
+
+public class new_clas {
+	
+	public static void myfunction(){
+		
+		System.out.println("I just created a new method in a new class");
+		
+		
+	}
+
+}
--- src/training/tutorial.java
+++ src/training/tutorial.java
@@ -5,5 +5,10 @@ public class tutorial {
 	public static void main(String args[]){
 		
 		System.out.println("Hello World");
+		
+		new_clas myclass = new new_clas();
+		
+		myclass.myfunction();
+		
 	}
 }
