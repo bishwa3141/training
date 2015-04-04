6264bfe5491be958e7cd5a33043cc06245585c75
bishwa3141 <bishwa.silwal@bison.howard.edu>
2015-04-04 14:28:13 -0400

    Learnt using array counters

--- src/training/tutorial.java
+++ src/training/tutorial.java
@@ -1,37 +1,27 @@
 
+
 package training;
-import java.util.Scanner;
+import java.util.Random;
+
 
 public class tutorial {
 	
-	public static void main(String args[]){
-		Scanner input = new Scanner(System.in);
-		
-		int firstnum, secondnum, thirdnum; 
-		
-		System.out.println("Enter the first number: ");
-		firstnum = input.nextInt();
-		
-		System.out.println("Enter the second number");
-		secondnum = input.nextInt();
-		
-		System.out.println("Enter the third number");
-		thirdnum = input.nextInt();
-		
-
-		if(firstnum > secondnum && firstnum > thirdnum)
-		{
-			System.out.println("First number is the biggest");
-		}
-		else if (secondnum > thirdnum)
-		{
-			System.out.println("Second number is the biggest");
-			
-		}
-		
-		else {
-			System.out.println("Third number is the largest");
-		}
-		
+	public static void main(String Args[]){
+	
+	int dicearray[] = new int[7];
+	
+	
+	Random rand = new Random();
+	
+	for(int roll = 1; roll < 1000; roll++)
+	{
+		++dicearray[1 + rand.nextInt(6)];
 	}
+		
+	for(int i = 0; i < dicearray.length; i++)
+	{
+		System.out.println(i + "\t" + dicearray[i]);
+	
+	}
+}
 }
